74bdceec996a80e852d60fd9ed214a635c93edb9
Youcef KEBIR <kebiryoucef07@gmail.com>
2021-06-02 14:17:11 +0100

    is_employee

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+
 User = get_user_model()
 
 
 # Create your models here.
+
+
 class EmployerProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # User = User.objects.filter(role="employer")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+
+
+User = get_user_model()
 
 
 class EmployeeProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # User =User.objects.filter(role="employee")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
